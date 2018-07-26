6052655111ca8ffb0ed2d3d534122c4e5116d37e
Michael Desa <mjdesa@gmail.com>
2018-07-26 15:17:01 -0400

    feat(http): add flux language http handler
    Additionally we added the top level routes to the /v2 api
    
    Co-authored-by: Andrew Watkins <andrew.watkinz@gmail.com>
    Co-authored-by: Michael Desa <mjdesa@gmail.com>

--- http/platform_handler.go
+++ http/platform_handler.go
@@ -2,6 +2,8 @@ package http
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	nethttp "net/http"
 	"strings"
 
@@ -19,6 +21,7 @@ type PlatformHandler struct {
 	AssetHandler         *AssetHandler
 	ChronografHandler    *ChronografHandler
 	SourceHandler        *SourceHandler
+	FluxLangHandler      *FluxLangHandler
 }
 
 func setCORSResponseHeaders(w nethttp.ResponseWriter, r *nethttp.Request) {
@@ -27,6 +30,37 @@ func setCORSResponseHeaders(w nethttp.ResponseWriter, r *nethttp.Request) {
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
 	}
+}
+
+var platformRoutesResponse = mustMarshalJSON(map[string]interface{}{
+	"sources": "/v2/sources",
+	"flux": map[string]string{
+		"self":        "/v2/flux",
+		"ast":         "/v2/flux/ast",
+		"suggestions": "/v2/flux/suggestions",
+	},
+	"chonograf":     "/chronograf/v1",
+	"layouts":       "/chronograf/v1/layouts",
+	"allUsers":      "/chronograf/v1/users",
+	"organizations": "/chronograf/v1/organizations",
+	"me":            "/chronograf/v1/me",
+	"environment":   "/chronograf/v1/env",
+	"mappings":      "/chronograf/v1/mappings",
+	"dashboards":    "/chronograf/v1/dashboards",
+	"dashboardsV2":  "/chronograf/v2/dashboards",
+	"cells":         "/chronograf/v2/cells",
+	"config": map[string]string{
+		"self": "/chronograf/v1/config",
+		"auth": "/chronograf/v1/config/auth",
+	},
+	"externalLinks": map[string]string{
+		"statusFeed": "https://www.influxdata.com/feed/json",
+	},
+})
+
+func (h *PlatformHandler) servePlatformRoutes(w nethttp.ResponseWriter, r *nethttp.Request) {
+	w.WriteHeader(nethttp.StatusOK)
+	w.Write(platformRoutesResponse)
 }
 
 // ServeHTTP delegates a request to the appropriate subhandler.
@@ -43,6 +77,17 @@ func (h *PlatformHandler) ServeHTTP(w nethttp.ResponseWriter, r *nethttp.Request
 		!strings.HasPrefix(r.URL.Path, "/v2/") &&
 		!strings.HasPrefix(r.URL.Path, "/chronograf/") {
 		h.AssetHandler.ServeHTTP(w, r)
+		return
+	}
+
+	// Serve the links base links for the API.
+	if r.URL.Path == "/v2/" || r.URL.Path == "/v2" {
+		h.servePlatformRoutes(w, r)
+		return
+	}
+
+	if strings.HasPrefix(r.URL.Path, "/v2/flux") {
+		h.FluxLangHandler.ServeHTTP(w, r)
 		return
 	}
 
@@ -104,4 +149,13 @@ func extractAuthorization(ctx context.Context, r *nethttp.Request) (context.Cont
 		return ctx, err
 	}
 	return idpctx.SetToken(ctx, t), nil
+}
+
+func mustMarshalJSON(i interface{}) []byte {
+	b, err := json.Marshal(i)
+	if err != nil {
+		panic(fmt.Sprintf("failed to marshal json: %v", err))
+	}
+
+	return b
 }
